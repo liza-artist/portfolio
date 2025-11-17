1f7232c899b6919ec50be0ca83231fd306b42546
liza-artist <lizaweb.07@gmail.com>
2025-11-17 17:47:15 +0300

    add styles and script

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ price.forEach((element, index) => {
   if (index % 2 !== 0) {
     element.classList.add('hidden-after');
   }});
+
+
+
+// поворот стрелки при выборе соц сети  
+const arrow = document.querySelector('.form__social-networks-arrow');
+const select = document.querySelector('.form__social-networks');
+
+let isRotated = false;
+
+function toggleRotation() {
+
+    isRotated = !isRotated;
+
+    arrow.style.transform = isRotated ? 'rotate(180deg)' : 'rotate(0deg)';
+}
+
+arrow.addEventListener('click', toggleRotation);
+select.addEventListener('click', toggleRotation);
